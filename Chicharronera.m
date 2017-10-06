85e04a17fa9f77c593522e8d175e8c0aef875ad3
gerryvinasL280899 <32582557+gerryvinasL280899@users.noreply.github.com>
2017-10-06 16:48:49 -0500

    prueba
    lo que sea

---
+++ Chicharronera.m
@@ -0,0 +1,9 @@
+function [ x1,x2 ] = Chicharronera( a,b,c )
+%Calcula las raíces de segundo orden
+%   mediante el uso de la formula general
+x1=((-b+((b^2)-(4*a*c))^(1/2))/(2*a))
+x2=((-b-((b^2)-(4*a*c))^(1/2))/(2*a))
+
+
+end
+
