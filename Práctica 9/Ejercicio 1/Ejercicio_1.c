2e7f750c8f85f4971b8c69fc57b5e2a6c61b86d8
gerryvinasL280899 <32582557+gerryvinasL280899@users.noreply.github.com>
2017-12-10 23:22:56 -0600

    .

---
+++ Práctica 9/Ejercicio 1/Ejercicio_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef struct lecturas{
+	float a_hombro;
+	float a_codo;
+	float a_munec;
+	char hora[70];
+	char fecha[90];
+};
+
+struct
+int main(int argc, char *argv[]) {
+	return 0;
+}
